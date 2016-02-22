c677275fa547555e9aa195c2b334aeb41c3ffe6c
Simon Liang <no.lhr0909@gmail.com>
2016-02-22 09:08:52 -0800

    adding resolver template styles

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 request a file from recipient(s) without having them reply with attachments
 
 # Components
-* nodejs server on elastic beanstalk
+* nodejs server on heroku
   * editor page that gives info to render the templates, this is when we put in the [variables](http://sdk.mixmax.com/docs/variables-in-templates) for recipient and file info
   * upload page that takes the upload to S3
-* a lambda job will kick in when S3 write happens and sends email back to the sender with download link
+* an AWS Lambda job that kicks in when S3 write happens and sends email back to the sender with download link
+
+# Setup
+Add an enhancement with following information:
+
+* Editor URL: https://mixmax-file-requests.herokuapp.com/editor
+* Resolver API URL: https://mixmax-file-requests.herokuapp.com/api/resolver
+
+# Next Steps
+* Editor accepting the edit query with data feedback
+* General UX of the whole feature, can request more text fields in the editor and make the file upload more sensible
+* Styling (Editor, Resolver Template, and the return email with file)
+* AWS IAM management (currently the IAM setup is a little messy, could clean up a bit and make it more secure)
+  * Use AWS Cognito service to allow requester to pre-sign a URL for the uploader to upload, so only the requester can download
--- api/resolver.jsx
+++ api/resolver.jsx
@@ -8,18 +8,57 @@ class UploadResolver extends React.Component {
   constructor(props) {
     super(props);
 
+    let fontFamily = "'proxima-nova','Avenir Next','Segoe UI','Calibri','Helvetica Neue',Helvetica,Arial,sans-serif";
+
     this.state = {
       caption: this.props.data.caption,
+      containerStyle: {
+        border: "1px solid #295191",
+        padding: "15px",
+        width: "400px",
+        borderRadius: "3px"
+      },
+      headerStyle: {
+        color: "#333",
+        fontFamily: fontFamily,
+        fontSize: "18px",
+        fontWeight: "600",
+        paddingTop: "6px",
+        lineHeight: "26px",
+        width: "100%"
+      },
+      captionStyle: {
+        color: "#aab",
+        fontSize: "12px",
+        fontFamily: fontFamily,
+        marginTop: "5px",
+        marginBottom: "5px"
+      },
+      uploadButtonStyle: {
+        backgroundColor: "#1155cc",
+        borderRadius: "3px",
+        padding: "6px 4px 4px 4px",
+        textAlign: "center"
+      },
+      uploadButtonTextStyle: {
+        fontFamily: fontFamily,
+        color: "white",
+        textTransform: "uppercase",
+        fontSize: "14px",
+        textDecoration: "none"
+      },
       url: "https://mixmax-file-requests.herokuapp.com/uploader?requester=!!!SENDER_EMAIL_URL!!!&provider=!!!EMAIL_ADDR_URL!!!"
     }
   }
 
   render() {
     return (
-      <div>
-        <div>!!!SENDER_EMAIL!!! says:</div>
-        <div>{ this.state.caption }</div>
-        <div><a href={ this.state.url } target="_blank">Upload Now</a></div>
+      <div className="preview-card" style={this.state.containerStyle}>
+        <div style={this.state.headerStyle}>!!!SENDER_EMAIL!!! asks you for a file:</div>
+        <div style={this.state.captionStyle}>{ this.state.caption }</div>
+        <div style={this.state.uploadButtonStyle}>
+          <a style={this.state.uploadButtonTextStyle} href={ this.state.url } target="_blank">Upload Now</a>
+        </div>
       </div>
     );
   }
